07f417aac2f9629b7d1477b9f2f3b2327970134a
Jefferson Leite Rodrigues Dantas <jefferson_rodrigues7@hotmail.com>
2023-10-16 23:18:24 -0300

    atualizando códigos
    atualizando códigos

--- RPG_Learn/Assets/Scripts/Player/PlayerController.cs
+++ RPG_Learn/Assets/Scripts/Player/PlayerController.cs
@@ -7,19 +7,18 @@ namespace RPG.Control
 {
     public class PlayerController : MonoBehaviour
     {
-        // Movement speeds for walking and running
+        // Velocidades de movimento
         [SerializeField] private float walkSpeed = 5.0f;
         [SerializeField] private float runSpeed = 10.0f;
         [SerializeField] private float rotationSpeed = 10.0f;
 
-        // Input and state variables
         private Vector2 movementInput;  // Armazena a entrada de movimento do jogador
 
         private Animator animator;
         private Rigidbody rb;           // Referência ao componente Rigidbody
         private PlayerInput playerInput;
 
-        private bool isWalking;
+        private bool isWalking = false;
         private int isWalkingHash;
 
         private bool isRunning = false; // Flag para determinar se o jogador está correndo
@@ -43,9 +42,9 @@ namespace RPG.Control
             // Registra callbacks para as entradas
             playerInput.Enable();
             playerInput.CharacterControls.Movement.performed += Move; // Callback de entrada de movimento
-            playerInput.CharacterControls.Movement.canceled += StopMoving; // Callback para parar o movimento
-            playerInput.CharacterControls.Run.started += StartRunning; // Callback para iniciar a corrida
-            playerInput.CharacterControls.Run.canceled += StopRunning; // Callback para parar a corrida
+            playerInput.CharacterControls.Movement.canceled += StopMove; // Callback para parar o movimento
+            playerInput.CharacterControls.Run.started += Run; // Callback para iniciar a corrida
+            playerInput.CharacterControls.Run.canceled += StopRun; // Callback para parar a corrida
         }
 
         private void OnDisable()
@@ -53,39 +52,9 @@ namespace RPG.Control
             // Cancela o registro de callbacks quando o script é desativado
             playerInput.Disable();
             playerInput.CharacterControls.Movement.performed -= Move;
-            playerInput.CharacterControls.Movement.canceled -= StopMoving;
-            playerInput.CharacterControls.Run.started -= StartRunning;
-            playerInput.CharacterControls.Run.canceled -= StopRunning;
-        }
-
-        private void Move(InputAction.CallbackContext context)
-        {    
-            isWalking = true;
-            movementInput = context.ReadValue<Vector2>(); // Obtém a entrada de movimento do contexto de entrada
-            animator.SetBool(isWalkingHash, true);
-            Debug.Log($"Movimentando {movementInput}");
-        }
-
-        private void StopMoving(InputAction.CallbackContext context)
-        {
-            isWalking = false;
-            movementInput = Vector2.zero; // Reseta a entrada de movimento quando o jogador para de se mover
-            animator.SetBool(isWalkingHash, false);
-            Debug.Log($"Parando de se movimentar {movementInput}");
-        }
-
-        private void StartRunning(InputAction.CallbackContext context)
-        {
-            isRunning = true; // Define a sinalização de corrida como verdadeira quando o jogador começa a correr
-            animator.SetBool(isRunningHash, true);
-            Debug.Log($"Começando a correr {isRunning}");
-        }
-
-        private void StopRunning(InputAction.CallbackContext context)
-        {
-            isRunning = false; // Define a sinalização de corrida como falsa quando o jogador para de correr
-            animator.SetBool(isRunningHash, false);
-            Debug.Log($"Parando de correr {isRunning}");
+            playerInput.CharacterControls.Movement.canceled -= StopMove;
+            playerInput.CharacterControls.Run.started -= Run;
+            playerInput.CharacterControls.Run.canceled -= StopRun;
         }
 
         private void FixedUpdate()
@@ -111,6 +80,45 @@ namespace RPG.Control
                 Quaternion newRotation = Quaternion.LookRotation(desiredMoveDirection);
                 transform.rotation = Quaternion.Slerp(transform.rotation, newRotation, rotationSpeed * Time.deltaTime);
             }
+        }
+
+        private void Move(InputAction.CallbackContext context)
+        {
+            isWalking = true;
+            movementInput = context.ReadValue<Vector2>(); // Obtém a entrada de movimento do contexto de entrada
+            animator.SetBool(isWalkingHash, true);
+            //Debug.Log($"Movimentando {movementInput}");
+        }
+
+        private void StopMove(InputAction.CallbackContext context)
+        {
+            isWalking = false;
+            movementInput = Vector2.zero; // Reseta a entrada de movimento quando o jogador para de se mover
+            stopRunAnimation();
+            animator.SetBool(isWalkingHash, false);
+            //Debug.Log($"Parando de se movimentar {movementInput}");
+        }
+
+        private void Run(InputAction.CallbackContext context)
+        {
+            if (isWalking)
+            {
+                isRunning = true; // Define a sinalização de corrida como verdadeira quando o jogador começa a correr
+                animator.SetBool(isRunningHash, true);
+                //Debug.Log($"Começando a correr {isRunning}");
+            }
+        }
+
+        private void StopRun(InputAction.CallbackContext context)
+        {
+            stopRunAnimation();
+        }
+
+        private void stopRunAnimation()
+        {
+            isRunning = false; // Define a sinalização de corrida como falsa quando o jogador para de correr
+            animator.SetBool(isRunningHash, false);
+            //Debug.Log($"Parando de correr {isRunning}");
         }
     }
 
