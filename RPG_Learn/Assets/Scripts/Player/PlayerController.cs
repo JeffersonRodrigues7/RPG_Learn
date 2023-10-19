2dd0ad5f2b425923403846ddd9ced0baa0f0b025
Jefferson Leite Rodrigues Dantas <jefferson_rodrigues7@hotmail.com>
2023-10-18 21:32:12 -0300

    movimento com botão do mouse
    movimento com botão do mouse

--- RPG_Learn/Assets/Scripts/Player/PlayerController.cs
+++ RPG_Learn/Assets/Scripts/Player/PlayerController.cs
@@ -1,7 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.AI;
 
 namespace RPG.Control
 {
@@ -24,6 +26,11 @@ namespace RPG.Control
         private bool isRunning = false; // Flag para determinar se o jogador está correndo
         private int isRunningHash;
 
+        public float stoppingDistance = 1.0f;
+        private NavMeshAgent navMeshAgent;
+        public Transform target;
+
+
         private void Awake()
         {
             playerInput = new PlayerInput();
@@ -33,6 +40,7 @@ namespace RPG.Control
         {
             animator = GetComponent<Animator>();
             rb = GetComponent<Rigidbody>(); // Obtém o componente Rigidbody anexado ao jogador
+            navMeshAgent = GetComponent<NavMeshAgent>();
             isWalkingHash = Animator.StringToHash("isWalking");
             isRunningHash = Animator.StringToHash("isRunning");
         }
@@ -59,27 +67,48 @@ namespace RPG.Control
 
         private void FixedUpdate()
         {
-            float currentSpeed = isRunning ? runSpeed : walkSpeed;
-            Vector3 movementInputDirection = new Vector3(movementInput.x, 0.0f, movementInput.y).normalized;
-
-            // Transforma a direção do movimento no espaço da câmera
-            Vector3 cameraForward = Camera.main.transform.forward;
-            Vector3 cameraRight = Camera.main.transform.right;
-            cameraForward.y = 0;
-            cameraRight.y = 0;
-            cameraForward.Normalize();
-            cameraRight.Normalize();
-            Vector3 desiredMoveDirection = cameraForward * movementInputDirection.z + cameraRight * movementInputDirection.x;
-
-            // Aplica a movimentação ao Rigidbody na direção da câmera
-            rb.velocity = desiredMoveDirection * currentSpeed;
-
-            // Rotação do jogador com base na direção do movimento
-            if (movementInputDirection != Vector3.zero)
+            if (Input.GetMouseButton(0))
             {
-                Quaternion newRotation = Quaternion.LookRotation(desiredMoveDirection);
-                transform.rotation = Quaternion.Slerp(transform.rotation, newRotation, rotationSpeed * Time.deltaTime);
+                Debug.Log("Here");
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                RaycastHit hit;
+
+                if (Physics.Raycast(ray, out hit))
+                {
+                    MoveToPosition(hit.point);
+                }
             }
+            else
+            {
+                navMeshAgent.ResetPath();
+                float currentSpeed = isRunning ? runSpeed : walkSpeed;
+                Vector3 movementInputDirection = new Vector3(movementInput.x, 0.0f, movementInput.y).normalized;
+
+                // Transforma a direção do movimento no espaço da câmera
+                Vector3 cameraForward = Camera.main.transform.forward;
+                Vector3 cameraRight = Camera.main.transform.right;
+                cameraForward.y = 0;
+                cameraRight.y = 0;
+                cameraForward.Normalize();
+                cameraRight.Normalize();
+                Vector3 desiredMoveDirection = cameraForward * movementInputDirection.z + cameraRight * movementInputDirection.x;
+
+                // Aplica a movimentação ao Rigidbody na direção da câmera
+                rb.velocity = desiredMoveDirection * currentSpeed;
+
+                // Rotação do jogador com base na direção do movimento
+                if (movementInputDirection != Vector3.zero)
+                {
+                    Quaternion newRotation = Quaternion.LookRotation(desiredMoveDirection);
+                    transform.rotation = Quaternion.Slerp(transform.rotation, newRotation, rotationSpeed * Time.deltaTime);
+                }
+            }
+
+        }
+
+        void MoveToPosition(Vector3 destination)
+        {
+            navMeshAgent.SetDestination(destination);
         }
 
         private void Move(InputAction.CallbackContext context)
